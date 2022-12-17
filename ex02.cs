d9ed21d6e58324860f6a22e96367f0545d04bbe9
Fotall <d.budarin@list.ru>
2022-12-17 14:56:28 +0300

    save

---
+++ ex02.cs
@@ -0,0 +1,16 @@
+// Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+int[] arrayFirst = { 4, 5, 2};
+int sumFirst = arrayFirst[0] + arrayFirst[1] + arrayFirst[2];
+Console.WriteLine(sumFirst);
+
+// 82 -> 10
+int[] arraySecond = { 8, 2};
+int sumSecond = arraySecond[0] + arraySecond[1];
+Console.WriteLine(sumSecond);
+
+// 9012 -> 12
+int[] arrayThird = { 9, 0, 1, 2};
+int sumThird = arrayThird[0] + arrayThird[1] + arrayThird[2] + arrayThird[3];
+Console.WriteLine(sumThird);
